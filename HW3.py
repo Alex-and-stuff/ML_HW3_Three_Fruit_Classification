af3006e505f842cf4bb22b47755314691c126a18
Alex C <pyps9804@gmail.com>
2022-05-25 22:17:38 +0800

    finish data preprocessing for train and test
    start writing nn

--- HW3.py
+++ HW3.py
@@ -2,16 +2,23 @@ from numpy import who
 from functions import *
 from sklearn.decomposition import PCA
 '''
-1. Import train data
+1. Import train data, test data
 '''
-train_data = np.zeros((SAMPLES*CLASS,DIM,DIM,DEPTH), dtype=np.uint8)
-label_data = np.zeros((SAMPLES*CLASS,1), dtype=np.uint8)
+
 fruits = ['Carambula', 'Lychee', 'Pear']
 
+train_data  = np.zeros((TRAINSAMP*CLASS,DIM,DIM,DEPTH), dtype=np.uint8)
+train_label = np.zeros((TRAINSAMP*CLASS,1), dtype=np.uint8)
 for idx, fruit in enumerate(fruits):
-    train_data[idx*SAMPLES:(idx+1)*SAMPLES] = ImportPic(fruits[idx])
-    label_data[idx*SAMPLES:(idx+1)*SAMPLES] = idx*np.ones((SAMPLES,1))
+    train_data[idx*TRAINSAMP:(idx+1)*TRAINSAMP] = ImportPic(fruits[idx])
+    train_label[idx*TRAINSAMP:(idx+1)*TRAINSAMP] = idx*np.ones((TRAINSAMP,1))
     # 0-489 490-979 980-1470
+
+test_data  = np.zeros((TESTSAMP*CLASS,DIM,DIM,DEPTH), dtype=np.uint8)
+test_label = np.zeros((TESTSAMP*CLASS,1), dtype=np.uint8)
+for idx, fruit in enumerate(fruits):
+    test_data[idx*TESTSAMP:(idx+1)*TESTSAMP] = ImportPic(fruits[idx], data_type='test')
+    test_label[idx*TESTSAMP:(idx+1)*TESTSAMP] = idx*np.ones((TESTSAMP,1))
 
 # Check if image is correct or not. If we wish to see the image, 
 # make sure the np array is dtype=np.uint8, else, use float
@@ -22,23 +29,23 @@ for idx, fruit in enumerate(fruits):
 2. PCA dimentional reduction to 2-dim
 '''
 pca = PCA(n_components=2)
-train_reduc = pca.fit_transform(train_data.reshape(SAMPLES*CLASS,DIM*DIM*DEPTH))
-
+train_reduc = pca.fit_transform(train_data.reshape(TRAINSAMP*CLASS,DIM*DIM*DEPTH))
+test_reduc  = pca.fit_transform(test_data.reshape(TESTSAMP*CLASS,DIM*DIM*DEPTH))
 '''
 3. Divide data to train and validation set
 '''
 # Concat reduced training data with its label (0-Carambula, 1-Lychee, 2-Pear)
-dataset = np.concatenate((train_reduc, label_data), axis=1)
+train_set = np.concatenate((train_reduc, train_label), axis=1)
+test_set  = np.concatenate((test_reduc, test_label), axis=1)
 # Shuffle the dataset
-np.random.shuffle(dataset)
-# Divide dataset to train_set and validation_set
-train_set, validation_set = np.split(dataset, [int(SAMPLES*CLASS*0.9)], axis=0)
+np.random.shuffle(train_set)  # [1470,3]
+np.random.shuffle(test_set)   # [495, 3]
 
 '''
 4. Neural Network implementation using sigmoid fcn 
    as nonlinear mapping and train weights with SGD
    I.   Input(1470*2) 
-   II.  Hidden layer
+   II.  Hidden layer (fc layer -> activation layer -> ...)
    III. Output layer
 '''
 
--- functions.py
+++ functions.py
@@ -1,15 +1,64 @@
 import cv2
 import numpy as np
 
-SAMPLES = 490
-CLASS   = 3
-DIM     = 32
-DEPTH   = 3
+TESTSAMP = 165
+TRAINSAMP = 490
+CLASS     = 3 
+DIM       = 32
+DEPTH     = 3
 
 def ImportPic(name, data_type = 'train'):
-    data = np.zeros((SAMPLES,DIM,DIM,DEPTH), dtype=np.uint8)
+    if data_type == 'train':
+        sample = TRAINSAMP
+    else:
+        sample = TESTSAMP
+
+    data = np.zeros((sample,DIM,DIM,DEPTH), dtype=np.uint8)
     path = f"Data/Data_{data_type}/{name}/{name}_{data_type}_"
-    for idx in range(490):
+    for idx in range(sample):
         im = cv2.imread(path+str(idx)+'.png')
-        data[idx] = im       
+        data[idx] = im    
     return data
+
+
+'''
+Fuctions for neural network
+'''
+# Activation fcn
+def sigmoid(x):
+    return 1.0/(1.0 + np.exp(-x))
+
+def sigmoid_prime(x):
+    return sigmoid(x)*(1-sigmoid(x))
+
+# Fully connected layer 
+class FClayer:
+    def __init__(self, input_dim, output_dim, batch_size):
+        self.input  = None
+        self.output = None
+        self.w = np.random.rand(input_dim, output_dim) - 0.5
+        self.b = np.random.rand(batch_size, output_dim) - 0.5
+    def forwardPropagation(self, input_data):
+        self.input  = input_data
+        self.output = np.dot(self.input, self.w) + self.b
+        return self.output
+    def backPropagation(self, error_dy, learning_rate):
+        error_dx = np.dot(self.input.T,error_dy)
+        error_dw = np.dot(error_dy,self.w.T)
+        error_db = error_dy
+        self.w  -= learning_rate*error_dw
+        self.b  -= learning_rate*error_db
+        return error_dx
+
+class ActivationLayer:
+    def __init__(self, act_fcn, act_fcn_prime):
+        self.input  = None
+        self.output = None
+        self.activation = act_fcn
+        self.activation_prime = act_fcn_prime
+    def forwardPropagation(self, input):
+        self.input = input
+        self.output = self.activation(self.input)
+        return self.output
+    def backPropagation(self, error_dy):
+        return self.activation_prime(self.input)*error_dy
